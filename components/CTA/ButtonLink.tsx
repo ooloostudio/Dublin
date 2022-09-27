8563706904fe42b261c70ad7b1d4df2e6be8a79c
ooloostudio <alejandro.haydar@thoughtindustries.com>
2022-09-27 12:27:16 -0400

    Update ButtonLink.tsx
    Update Button

--- components/CTA/ButtonLink.tsx
+++ components/CTA/ButtonLink.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from '../../renderer/share.svg';
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/solid'
 
 const ButtonLink = (props: { text: string; linkUrl: string }) => {
   return (
     <div className="m-8 flex justify-center">
       <a href={props.linkUrl} className="">
-        <button className="flex flex-row justify-center shadow-md rounded-full bg-brandSecondary-500 text-textPrimary-300 font-normal text-lg w-80 h-11 py-2 text-center sm:text-sm">
+        <button className="flex flex-row justify-center al shadow-md rounded-full bg-brandSecondary-500 text-textPrimary-300 font-normal text-lg w-80 h-11 py-2 text-center sm:text-sm">
           {props.text}
-          <img src={logo} className="pl-2" />
+          <ArrowTopRightOnSquareIcon className="h-7 w-7 p-1"/>
         </button>
       </a>
     </div>
