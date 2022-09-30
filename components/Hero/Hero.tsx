4839c6ccf804a7dc0eb48aa1c2926c5d288be13e
ooloostudio <alejandro.haydar@thoughtindustries.com>
2022-09-30 16:52:27 -0400

    Update Hero.tsx

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = (props: { headline: string; body: string; buttonUrl: string; button
             <div className=" mx-auto mt-5 px-20 text-lg text-indigo-200 md:mt-8 text-center sm:text-sm">{props.body}</div>
             <div className="flex flex-col justify-center items-center mt-8 px-24 text-center md:flex-row">
                 <span className="relative inline-flex w-full md:w-auto ">
-                    <a href={props.buttonUrl} type="button" className=" inline-flex items-center justify-center w-full px-8 py-4 text-lg font-medium leading-6 text-textPrimary-400 bg-brandSecondary-500 border border-transparent rounded-md md:w-auto hover:bg-brandSecondary-300 hover:animate-pulse focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 sm:text-sm">
+                    <a href={props.buttonUrl} type="button" className="inline-flex items-center justify-center w-full px-8 py-4 text-lg font-medium leading-6 text-textPrimary-400 bg-brandSecondary-500 border border-transparent rounded-md md:w-auto hover:bg-brandSecondary-300 hover:animate-pulse focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 sm:text-sm">
                     {props.buttonText}
                     </a>
                     <span className="absolute  top-0 right-0 px-2 py-1 -mt-3 -mr-6 text-xs font-medium leading-tight text-white bg-green-400 rounded-full">only $15/mo</span>
